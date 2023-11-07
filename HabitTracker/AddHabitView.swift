f9af1c52c290f9a6f5447f51aa49dd7dbe16a46b
Scott Crowder <scrowder@gmail.com>
2023-11-07 09:35:51 -0600

    create file

---
+++ HabitTracker/AddHabitView.swift
@@ -0,0 +1,18 @@
+//
+//  AddHabitView.swift
+//  HabitTracker
+//
+//  Created by SCOTT CROWDER on 11/7/23.
+//
+
+import SwiftUI
+
+struct AddHabitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddHabitView()
+}
