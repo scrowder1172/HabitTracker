54557bfbd437a2b9194f4653fe69931f8eb201fe
Scott Crowder <scrowder@gmail.com>
2023-11-07 09:23:49 -0600

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by SCOTT CROWDER on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
