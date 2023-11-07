f79c16ab39854cc7ba480b798966ad258eebbe44
Scott Crowder <scrowder@gmail.com>
2023-11-07 11:48:27 -0600

    create Habits class to track habits

--- HabitTracker/Habits.swift
+++ HabitTracker/Habits.swift
@@ -5,4 +5,40 @@
 //  Created by SCOTT CROWDER on 11/7/23.
 //
 
-import Foundation
+import SwiftUI
+
+enum HabitType: Codable {
+    case Personal, Business, Health, School
+}
+
+struct Habit: Identifiable, Codable {
+    var id: UUID = UUID()
+    let name: String
+    let purpose: String
+    let type: HabitType
+    let dateAdded: Date
+    let dateLastUpdated: Date
+}
+
+@Observable
+class Habits {
+    var habits: [Habit] = [Habit]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(habits) {
+                UserDefaults.standard.set(encoded, forKey: "Habits")
+            }
+        }
+    }
+    
+    init() {
+        if let savedHabits = UserDefaults.standard.data(forKey: "Habits") {
+            if let decodedHabits = try? JSONDecoder().decode([Habit].self, from: savedHabits) {
+                habits = decodedHabits
+                return
+            }
+        }
+        
+        // return an empty array if previous habits could not be found or loaded
+        habits = []
+    }
+}
