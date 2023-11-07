1a6db4face51509436c7f77758a93dc3b5cc59fa
Scott Crowder <scrowder@gmail.com>
2023-11-07 16:15:40 -0600

    Created custom structs to separate out form content

--- HabitTracker/AddHabitView.swift
+++ HabitTracker/AddHabitView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct AddHabitView: View {
-    @State private var habitName: String = ""
-    @State private var purpose: String = ""
+    @State private var habitTitle: String = ""
+    @State private var description: String = ""
     @State private var targetDate: Date = Date()
     @State private var creationDate: Date = Date()
     @State private var habitType: HabitType = .personal
@@ -21,52 +21,22 @@ struct AddHabitView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                HabitFormTextField(text: $habitName, label: "name")
+                HabitFormTextField(text: $habitTitle, label: "title")
                     .padding(.top, 30)
-                HabitFormTextField(text: $purpose, label: "purpose")
-                VStack {
-                    HStack {
-                        Text("Habit Type")
-                        Picker("Type", selection: $habitType) {
-                            ForEach(HabitType.allCases, id: \.self) { habitType in
-                                Text(habitType.rawValue)
-                            }
-                        }
-                    }
-                }
-                .frame(width: 350, height: 50)
-                .overlay(
-                            RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
-                                .stroke(Color.gray, lineWidth: 1) // Adjust border color and line width
-                        )
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
-                .pickerStyle(DefaultPickerStyle())
                 
-                VStack {
-                    Text("Due Date")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    DatePicker("Due Date", selection: $targetDate)
-                        .datePickerStyle(GraphicalDatePickerStyle())
-                }
-                    .padding(.horizontal, 20)
-                    .frame(width: 350, height: 425)
-                    .overlay(
-                                RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
-                                    .stroke(Color.gray, lineWidth: 1) // Adjust border color and line width
-                            )
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 10)
+                HabitFormPicker(selection: $habitType, pickerStyle: SegmentedPickerStyle())
                 
+                HabitFormTextEditor(text: $description, label: "purpose")
+                
+                HabitFormDatePicker(date: $targetDate)
             }
-            .frame(maxHeight: 700)
             .navigationTitle("Create a New Habit")
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarBackButtonHidden()
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
                     Button("Save") {
-                        let newHabit: HabitItem = HabitItem(name: habitName, purpose: purpose, type: habitType, targetDate: targetDate, dateAdded: creationDate, dateLastUpdated: creationDate)
+                        let newHabit: HabitItem = HabitItem(title: habitTitle, description: description, type: habitType, targetDate: targetDate, dateAdded: creationDate, dateLastUpdated: creationDate)
                         habits.habit.append(newHabit)
                         dismiss()
                     }
@@ -77,8 +47,6 @@ struct AddHabitView: View {
                     }
                 }
             }
-            Text(creationDate.formatted(date: .abbreviated, time: .shortened))
-            Spacer()
         }
     }
 }
@@ -92,8 +60,9 @@ struct HabitFormTextField: View {
                 .font(.caption)
                 .foregroundStyle(Color.indigo)
             TextField(label, text: $text)
+            
         }
-        .padding(.horizontal, 50)
+        .padding(.horizontal, 20)
         .frame(height: 50)
         .overlay(
                     RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
@@ -102,6 +71,77 @@ struct HabitFormTextField: View {
         .padding(.horizontal, 20)
         .padding(.vertical, 10)
         //.padding(.vertical, 10)
+    }
+}
+
+struct HabitFormTextEditor: View {
+    @Binding var text: String
+    var label: String
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(label)
+                .font(.caption)
+                .foregroundStyle(Color.indigo)
+            TextEditor(text: $text)
+                .frame(height: 100)
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 150)
+        .overlay(
+                    RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
+                        .stroke(Color.gray, lineWidth: 1) // Adjust border color and line width
+                )
+        .padding(.horizontal, 20)
+        .padding(.vertical, 10)
+        //.padding(.vertical, 10)
+    }
+}
+
+struct HabitFormPicker<Style>: View where Style: PickerStyle{
+    @Binding var selection: HabitType
+    var pickerStyle: Style
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("habit type")
+                .font(.caption)
+                .foregroundStyle(Color(.indigo))
+                .padding(.leading, 20)
+            Picker("Type", selection: $selection) {
+                ForEach(HabitType.allCases, id: \.self) { habitType in
+                    Text(habitType.rawValue)
+                }
+            }
+            .padding(.horizontal, 20)
+            .pickerStyle(pickerStyle)
+        }
+        .frame(width: 350, height: 75)
+        .overlay(
+                    RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
+                        .stroke(Color.gray, lineWidth: 1) // Adjust border color and line width
+                )
+        .padding(.horizontal, 20)
+        .padding(.vertical, 10)
+    }
+}
+
+struct HabitFormDatePicker: View {
+    @Binding var date: Date
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("Target Date")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            DatePicker("Target Date", selection: $date)
+                .datePickerStyle(GraphicalDatePickerStyle())
+        }
+            .padding(.horizontal, 20)
+            .frame(width: 350, height: 425)
+            .overlay(
+                        RoundedRectangle(cornerRadius: 10) // Adjust corner radius for desired curvature
+                            .stroke(Color.gray, lineWidth: 1) // Adjust border color and line width
+                    )
+            .padding(.horizontal, 20)
+            .padding(.vertical, 10)
     }
 }
 
